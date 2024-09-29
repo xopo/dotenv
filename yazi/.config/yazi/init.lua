9bb9a7edf82a49b0dcc1ebf4c660a5e79bd1e99e
opo <opo@local>
2024-09-29 12:49:20 +0300

    add aerospace, update nvim & yazi

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,6 +1,6 @@
 require("full-border"):setup()
 
-function Linemode:my_own()
+function Linemode:custom()
 	local year = os.date("%Y")
 	local time = (self._file.cha.modified or 0) // 1
 	time = os.date("%d %b %Y", time)
@@ -23,6 +23,19 @@ function Linemode:my_own()
 	})
 end
 
+Status:children_add(function()
+  local h = cx.active.current.hovered
+  if not h or not ya.user_name then
+    return ui.Line {}
+  end
+
+  return ui.Line {
+    ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+    ui.Span(":"),
+    ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+    ui.Span(" "),
+  }
+end, 500, Status.RIGHT)
 -- local old_linemode = Folder.linemod
 -- function linemode(area)
 -- 	if cx.active.conf.linemode ~= "my-own" then
