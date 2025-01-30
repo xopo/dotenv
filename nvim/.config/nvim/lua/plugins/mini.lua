1ff78b7a573db4ce69bae7d5a795537f922d3f79
opo <opo@local>
2025-01-30 08:44:30 +0200

    Add tmux - nvim integration

---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,12 @@
+return {
+    "echasnovski/mini.nvim",
+    enabled = true,
+    version = false,
+    -- config = function()
+    --     require("mini.ai").setup()
+    --     require("mini.suround").setup()
+    --     require("mini.operators").setup()
+    --     require("mini.pairs").setup()
+    --     require("mini.bracketed").setup()
+    -- end,
+}
