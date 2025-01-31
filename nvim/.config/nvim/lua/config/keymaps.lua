e9e53b9a06bc44080ec20a7eed279ff4922906e0
opo <opo@local>
2025-01-31 09:07:01 +0200

    - customizing stuff like the battery icon

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,14 +3,6 @@
 -- Add any additional keymaps here
 
 local map = vim.keymap.set
--- cnext ancprev change on quicklist
-map("n", "˚", "<cmd>cprev<CR>", { desc = "prev quick list element" })
-map("n", "∆", "<cmd>cnext<CR>", { desc = "next quick list element" })
-map("n", "[c", "<cmd>cprev<CR>", { desc = "prev quick list element" })
-map("n", "]c", "<cmd>cnext<CR>", { desc = "next quick list element" })
-
--- resource for lua stuff
-map("n", "<space><space>x", "<cmd>source %<CR>")
 
 -- Oil speed shortcut
 -- map("n", "-", "<CMD>Yazi<CR>", { desc = "Open parent directory" })
--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -2,11 +2,12 @@ return {
     "echasnovski/mini.nvim",
     enabled = true,
     version = false,
-    -- config = function()
-    --     require("mini.ai").setup()
-    --     require("mini.suround").setup()
-    --     require("mini.operators").setup()
-    --     require("mini.pairs").setup()
-    --     require("mini.bracketed").setup()
-    -- end,
+    config = function()
+        print("Inside mini, install ai, surround, operators, pairs, bracketed")
+        require("mini.ai").setup()
+        require("mini.surround").setup()
+        require("mini.operators").setup()
+        require("mini.pairs").setup()
+        require("mini.bracketed").setup()
+    end,
 }
