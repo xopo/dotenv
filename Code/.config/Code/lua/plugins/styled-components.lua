24b405162be997942a882b875d475a18d2d9ad4b
opo <opo@local>
2025-01-31 12:22:28 +0200

    - remove Code, update tmux

--- Code/.config/Code/lua/plugins/styled-components.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "styled-components/vim-styled-components",
-  event = { "VeryLazy", "BufRead" },
-  lazy = true,
-  ft = "ts.tsx",
-  -- config = function()
-  --   print("loading styled components")
-  -- end,
-}
