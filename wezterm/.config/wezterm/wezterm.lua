b50841722367b25161c58b630c6133f141b419b5
opo <opo@local>
2025-03-19 08:27:28 +0200

    - latest update
    - use new .tmux.conf smaller less chunky
    - save current lazy-lock which looks stable

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,4 +6,22 @@ config.send_composed_key_when_right_alt_is_pressed = true
 config.enable_scroll_bar = true
 -- config.link_opens_in_browser = true
 
+wezterm.on("update-status", function(window) --, _pane)
+	local overrides = window:get_config_overrides() or {}
+	if window:is_focused() then
+		overrides.colors = {
+			tab_bar = {
+				background = "#ff0000", --#1e1e1e",
+			},
+		}
+	else
+		overrides.colors = {
+			tab_bar = {
+				background = "#0000",
+			},
+		}
+	end
+	window:get_config_overrides(overrides)
+end)
+
 return config
