c9ece134f29bfc5716c9c1c41add92e901819d9d
opo <opo@local>
2025-07-11 09:19:09 +0300

    - latest local

--- nvim/.config/nvim/lua/plugins/dev.lua
+++ nvim/.config/nvim/lua/plugins/dev.lua
@@ -1,34 +1,31 @@
 return {
+    { 'mason-org/mason.nvim', version = '^2.0.0' },
     {
-        dir = "~/learn/lua/fzf-worktree.nvim/dev",
+        dir = '~/learn/lua/fzf-worktree.nvim/dev',
         enabled = false,
-        event = "VeryLazy",
+        event = 'VeryLazy',
         config = function()
-            print("loading worktree")
-            require("fzf-worktree")
+            print('loading worktree')
+            require('fzf-worktree')
         end,
     },
     {
-        dir = "~/learn/lua/present.nvim",
+        dir = '~/learn/lua/present.nvim',
         enabled = false,
-        event = "VeryLazy",
-        config = function()
-            require("present")
-        end,
+        event = 'VeryLazy',
+        config = function() require('present') end,
     },
     {
-        "ray-x/go.nvim",
+        'ray-x/go.nvim',
         enabled = false,
         dependencies = { -- optional packages
-            "ray-x/guihua.lua",
-            "neovim/nvim-lspconfig",
-            "nvim-treesitter/nvim-treesitter",
+            'ray-x/guihua.lua',
+            'neovim/nvim-lspconfig',
+            'nvim-treesitter/nvim-treesitter',
         },
-        config = function()
-            require("go").setup()
-        end,
-        event = { "CmdlineEnter" },
-        ft = { "go", "gomod" },
+        config = function() require('go').setup() end,
+        event = { 'CmdlineEnter' },
+        ft = { 'go', 'gomod' },
         build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     },
 }
